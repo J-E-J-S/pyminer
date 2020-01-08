c897b34861056794e5cbe33c3752f9f359de7163
George Pearse <georgepearse@kubrickgroup.com>
2020-01-08 08:24:44 +0000

    ''

--- GeorgeMiner.py
+++ GeorgeMiner.py
@@ -5,7 +5,13 @@ Created on Sun Dec 29 17:56:58 2019
 @author: James Sanders and George Pearse
 """
 
+# %% Debugging / finding modules / extending os path 
 
+import sys 
+import os 
+
+print(sys.path) 
+sys.path.append(r"C:\\Users\\GeorgePearse\\Anaconda3\\Lib\\site-packages")
 # %%
 
 from bs4 import BeautifulSoup
@@ -14,29 +20,19 @@ import nltk.tokenize as tokenize
 import os
 import nltk
 import pandas as pd
-import xml.etree.ElementTree as ET
-
-# %% Main Tools
-
-def get_title(file_path):
-    ''' this function is incomplete and not required for the basic functionality of the tool'''
-    f = open(file_path, 'r') # literally just opens the file
-    tree = ET.ElementTree(f)
-    root = tree.getroot()
-    for elem in tree.iterfind('journal-article'):
-        print(elem)
-
-#get_title(r"C:\Users\James\Documents\george_test\PMC5562215\fulltext.xml")
 
 # %%
-
+def send_title(title):
+    return title
 
 def get_frequency(file_path):
     '''takes in xml file path and outputs list of most common words'''
     f = open(file_path, 'r')
-    soup = BeautifulSoup(f, 'lxml') # Beautiful soup takes the code written to format some piece of text and turns it into nested objects you can navigate
+    soup = BeautifulSoup(f, 'html.parser') # Beautiful soup takes the code written to format some piece of text and turns it into nested objects you can navigate
     f.close()
     text = soup.text # This is extracting just the written text and removing the pieces of code that purely exist for formatting
+    title = soup.find('article-title').text # extracts the title to act as first column of DataFrame 
+    send_title(title) 
     custom_list = ['The'] # Can also edit this (add more words) -> other words that you don't want to count (words that aren't included in stopwords)
     stop_words = list(stopwords.words('english')) + custom_list
     list_of_words = text.split(' ') # converting the body of text into a list of distinct words
@@ -47,7 +43,8 @@ def get_frequency(file_path):
             filtered_text.append(i) # removing stop words from the text by adding only those that are not in stop words
     for i in filtered_text:
         key_words[i] = key_words.get(i,0) + 1 # counter to find the number of times that any words occur
-    return key_words
+    return key_words, title
+
 
 def get_most_common_words(key_words):
     '''sorts the list of most common words'''
@@ -74,14 +71,14 @@ def get_matched(search_list, words_frequency):
 
 def do_everything(file_path, search_list):
     '''just combining lots of the earlier functions'''
-    key_words = get_frequency(file_path)
+    key_words, title = get_frequency(file_path)
     most_common_words = get_most_common_words(key_words)
     frequency = get_frequency_sorted(most_common_words,key_words)
     matched = get_matched(search_list,frequency)
     score = 0
     for match in matched:
         score = score + match[1] # scoring an article based on how strongly it matches the your selected keywords
-    return (score, matched)
+    return (title, score, matched)
 
 
 # %% Batch Management
@@ -97,11 +94,12 @@ def read_folder(folder_path, search_list):
     results_df = pd.DataFrame(results) # create a DataFrame (table) of the results
     resultsTRANPOSE = results_df.T # transposing the DataFrame so that it is more easily read
     sorted_df = resultsTRANPOSE.sort_values(by=[0],ascending=False)
+    sorted_df.columns = ['Article Title','Score','Keywords']
     return sorted_df
 
 # %%
 
 search_list = ['FAD', 'FMN', 'LOV', 'Cysteine', 'transcription', 'factor', 'blue']
-folder_path = r"/c/pUsers/James/documents/GeorgesMarvelousMiner/test_1"
+folder_path = r"C:\Users\GeorgePearse\projects\James\GeorgesMarvelousMiner\Test_1"
 
 results = read_folder(folder_path,search_list) # this combines all the earlier pieces of code into one command
